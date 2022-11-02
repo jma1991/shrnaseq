58ab6ad571db1cb936d04db113b284ae43b6775d
claireprince <Claire.Prince@zifornd.com>
2022-11-02 11:49:58 +0000

    Update ge post deploy script

--- workflow/envs/ge.post-deploy.sh
+++ workflow/envs/ge.post-deploy.sh
@@ -1,3 +1,5 @@
 Rscript -e 'install.packages("BiocManager", repos = "http://cran.us.r-project.org")'
 Rscript -e 'BiocManager::install(version = "3.15")'
-Rscript -e 'BiocManager::install(c("AnnotationDbi", "limma", "BiocGenerics", "edgeR",  "GO.db"))'
+Rscript -e 'BiocManager::install(c("limma", "edgeR"))'
+Rscript -e 'BiocManager::install(c("Biobase", "BiocGenerics", "S4Vectors", "IRanges"))'
+Rscript -e 'BiocManager::install(c("AnnotationDbi", "GO.db"))'
